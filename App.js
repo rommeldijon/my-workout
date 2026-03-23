5b7df4463de1054828dff2604ae796992a04b0f6
Rommel Dijon <rdijon@ymail.com>
2026-03-23 12:05:08 -0700

    Updatd feat(navigation): add stack navigation for login and signup screens

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import LoginScreen from "./src/screens/LoginScreen";
+import SignupScreen from "./src/screens/SignupScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <LoginScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen 
+          name="Signup" 
+          component={SignupScreen} 
+          options={{ headerShown: false }}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
