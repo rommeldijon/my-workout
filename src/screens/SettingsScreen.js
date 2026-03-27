83a16b14e7a8d38f98dbfe843a4b02a32e40584f
Rommel Dijon <rdijon@ymail.com>
2026-03-27 10:39:59 -0700

    Added styles to SettingsScreen.js

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,18 +1,3 @@
-import React from "react";
-import { View, Text, Switch, StyleSheet } from "react-native";
-
-const SettingsScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Settings</Text>
-
-      <Text>Dark Mode</Text>
-      <Switch />
-
-      <Text>Notifications</Text>
-      <Switch />
-    </View>
-  );
-};
-
-export default SettingsScreen;
+flexDirection: "row",
+justifyContent: "space-between",
+alignItems: "center",
